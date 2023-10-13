d57849dd5de1af3940ceaaabf39e40215ddf5d8c
NoNicks <viniciusnoup@gmail.com>
2023-10-13 14:56:08 -0300

    quase

--- treco.js
+++ treco.js
@@ -21,34 +21,34 @@ celciusInput.addEventListener('input', function(){
 })
 
 fahrenheitInput.addEventListener('input', function(){
-    let cTemp = parseFloat(celciusInput.value)
-    let fTemp = (cTemp*(9/5)) + 32
-    let kTemp = cTemp + 273.15
-    let rTemp = (cTemp*9/5) + 491.67
+    let fTemp = parseFloat(fahrenheitInput.value)
+    let cTemp = (fTemp - 32) * 5/9
+    let kTemp = (fTemp - 32) * 5/9 + 273.15
+    let rTemp = fTemp + 491.67
 
-    fahrenheitInput.value = roundNumber(fTemp)
+    fahrenheitInput.value = roundNumber(cTemp)
     kelvinInput.value = roundNumber(kTemp)
     rankineInput.value = roundNumber(rTemp)
 })
 
 kelvinInput.addEventListener('input', function(){
-    let cTemp = parseFloat(celciusInput.value)
-    let fTemp = (cTemp*(9/5)) + 32
-    let kTemp = cTemp + 273.15
-    let rTemp = (cTemp*9/5) + 491.67
+    let kTemp = parseFloat(kelvinInput.value)
+    let cTemp = kTemp - 273.15
+    let fTemp = (kTemp - 273.15) * (9/5) + 32
+    let rTemp = kTemp*1.8
 
+    celciusInput.value = roundNumber(cTemp)
     fahrenheitInput.value = roundNumber(fTemp)
-    kelvinInput.value = roundNumber(kTemp)
     rankineInput.value = roundNumber(rTemp)
 })
 
 rankineInput.addEventListener('input', function(){
-    let cTemp = parseFloat(celciusInput.value)
-    let fTemp = (cTemp*(9/5)) + 32
-    let kTemp = cTemp + 273.15
-    let rTemp = (cTemp*9/5) + 491.67
+    let rTemp = parseFloat(rankineInput.value)
+    let cTemp = (rTemp - 491.67) * 5/9
+    let fTemp = rTemp - 459.67
+    let kTemp = (rTemp * 5/9)
 
+    celciusInput.value = roundNumber(cTemp)
     fahrenheitInput.value = roundNumber(fTemp)
     kelvinInput.value = roundNumber(kTemp)
-    rankineInput.value = roundNumber(rTemp)
 })
